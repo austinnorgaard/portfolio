529c99394b06504ff5198d1c8650eb5a9a402177
Austin Norgaard <austinnorgaard@gmail.com>
2023-03-10 09:52:03 -0800

    Updated Contact Info

--- src/components/info-page/info-page.js
+++ src/components/info-page/info-page.js
@@ -4,7 +4,7 @@ import github from '../../github.png';
 import linkedin from '../../linkedin.png';
 import instagram from '../../instagram.png';
 
-const email = "norgaustin@gmail.com";
+const email = "me@austinnorgaard.com";
 
 function Info() {
     return (
@@ -24,7 +24,7 @@ function Info() {
             <Box as="main" id="main">
                 <Box as="h1" id="content">Contact Info:
                     <Box as="p">austin norgaard</Box>
-                    <Box as="p">email: <Link id="link" to={'mailto:' + email}>norgaustin@gmail.com</Link></Box>
+                    <Box as="p">email: <Link id="link" to={'mailto:' + email}>me@austinnorgaard.com</Link></Box>
                     <Box as="p">phone: 503-987-2311</Box>
                 </Box>
                 <Box as="h1" id="links">Links:<br/>
